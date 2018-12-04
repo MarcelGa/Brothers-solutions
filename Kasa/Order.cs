29b4f329bc5932dc70712de6695b045520a200ca
Marcel Galovič <galovic.marcel@gmail.com>
2018-12-04 22:52:14 +0100

    FormMain - premenovanie Order - Doplnenie komentárov

--- Kasa/Order.cs
+++ Kasa/Order.cs
@@ -10,15 +10,29 @@ namespace Kasa
 	/// </summary>
 	public class Order
 	{
+		public Order(){}
+		/// <summary>
+		/// Konštruktor objektu
+		/// </summary>
+		/// <param name="items"></param>
 		public Order(List<Item> items)
 		{
 			Items = items;
 			Date = DateTime.Now;
 		}
+		/// <summary>
+		/// Zoznam položiek objednávky
+		/// </summary>
 		[XmlArrayItem]
 		public List<Item> Items { get; set; }
+		/// <summary>
+		///Dátum uzavretia objednávky
+		/// </summary>
 		[XmlAttribute]
 		public DateTime Date { get; set; }
+		/// <summary>
+		/// Výsledná cena objednávky
+		/// </summary>
 		[XmlIgnore]
 		public decimal Price => Items.Sum(i => i.Price);
 	}
